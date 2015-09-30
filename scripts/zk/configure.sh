3bdecf124314c60f2ad579b51b652b6813bfd89e
Xavier DEGENNE <xavier.degenne@fastconnect.fr>
2015-09-30 16:22:41 +0000

    clean up

--- scripts/zk/configure.sh
+++ scripts/zk/configure.sh
@@ -4,5 +4,5 @@ echo "`date` [$$@`hostname`] zookeeper - configure" >> /tmp/hss.log
 for l in `set | egrep 'INSTANCE|NODE'`; do echo "`date` [$$@`hostname`] $l" >> /tmp/hss.log; done
 echo "`date` [$$@`hostname`] `set | grep 'ip_address'`" >> /tmp/hss.log
 export INPUTNAME=ip_address
-IFS=',' ;for i in $INSTANCES; do varname="${i}_${INPUTNAME}"; echo "the ${INPUTNAME} value for ${i} is ${!varname}"; done
+IFS=',' ;for i in $INSTANCES; do varname="${i}_${INPUTNAME}"; echo "`date` [$$@`hostname`] The ${INPUTNAME} value for ${i} is ${!varname}" >> /tmp/hss.log; done
 sleep $[ ( $RANDOM % 9 )  + 1 ]s
